19def10b50ab569f12c65a458621ce598b5c4273
Maurice Oluoch <53207394+blvck-code@users.noreply.github.com>
2024-03-26 22:57:26 +0300

    working with class context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 class BookList extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>the way of kings</li>
-          <li>the name of the wild</li>
-          <li>the final empire</li>
+          <li style={{ background: theme.ui, color: theme.syntax }}>
+            the way of kings
+          </li>
+          <li style={{ background: theme.ui, color: theme.syntax }}>
+            the name of the wild
+          </li>
+          <li style={{ background: theme.ui, color: theme.syntax }}>
+            the final empire
+          </li>
         </ul>
       </div>
     );
